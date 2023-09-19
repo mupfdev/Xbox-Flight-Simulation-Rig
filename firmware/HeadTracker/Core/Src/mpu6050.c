465f92b69897bb8cddb95fbdc83a294ac2b68fc7
Michael Fitzmayer <mail@michael-fitzmayer.de>
2023-09-20 00:12:53 +0200

    Minor cleanup, adjust casing

--- firmware/HeadTracker/Core/Src/mpu6050.c
+++ firmware/HeadTracker/Core/Src/mpu6050.c
@@ -11,15 +11,15 @@
 #include "mpu6050.h"
 #include "stm32f1xx_hal.h"
 
-#define MPU6050_ADDR         (0x68 << 1)
-#define MPU6050_SMPLRT_DIV   0x19
-#define MPU6050_CONFIG       0x1a
-#define MPU6050_gyro_CONFIG  0x1b
-#define MPU6050_acc_EL_CONFIG 0x1c
-#define MPU6050_WHO_AM_I     0x75
-#define MPU6050_PWR_MGMT_1   0x6b
-#define MPU6050_TEMP_H       0x41
-#define MPU6050_TEMP_L       0x42
+#define MPU6050_ADDR          (0x68 << 1)
+#define MPU6050_SMPLRT_DIV    0x19
+#define MPU6050_CONFIG        0x1a
+#define MPU6050_GYRO_CONFIG   0x1b
+#define MPU6050_ACC_EL_CONFIG 0x1c
+#define MPU6050_WHO_AM_I      0x75
+#define MPU6050_PWR_MGMT_1    0x6b
+#define MPU6050_TEMP_H        0x41
+#define MPU6050_TEMP_L        0x42
 
 int init_mpu6050(I2C_HandleTypeDef* i2c_handle, mpu6050_t* mpu6050)
 {
@@ -45,9 +45,9 @@ int init_mpu6050(I2C_HandleTypeDef* i2c_handle, mpu6050_t* mpu6050)
     HAL_I2C_Mem_Write(i2c_handle, MPU6050_ADDR, MPU6050_SMPLRT_DIV,   1, &data, 1, 100);
     HAL_I2C_Mem_Write(i2c_handle, MPU6050_ADDR, MPU6050_CONFIG,       1, &data, 1, 100);
     data = 0x08;
-    HAL_I2C_Mem_Write(i2c_handle, MPU6050_ADDR, MPU6050_gyro_CONFIG,  1, &data, 1, 100);
+    HAL_I2C_Mem_Write(i2c_handle, MPU6050_ADDR, MPU6050_GYRO_CONFIG,  1, &data, 1, 100);
     data = 0x00;
-    HAL_I2C_Mem_Write(i2c_handle, MPU6050_ADDR, MPU6050_acc_EL_CONFIG, 1, &data, 1, 100);
+    HAL_I2C_Mem_Write(i2c_handle, MPU6050_ADDR, MPU6050_ACC_EL_CONFIG, 1, &data, 1, 100);
     data = 0x01;
     HAL_I2C_Mem_Write(i2c_handle, MPU6050_ADDR, MPU6050_PWR_MGMT_1,   1, &data, 1, 100);
 
